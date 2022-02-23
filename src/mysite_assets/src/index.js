bfe671a412afc0f22d6721be31fecba3b6deb51e
System Administrator <root@KKdeMacBook-Air.local>
2022-02-23 14:06:43 +0800

    delete useless content

--- src/mysite_assets/src/index.js
+++ src/mysite_assets/src/index.js
@@ -1,12 +1,2 @@
 import "./js/index"
 import "./css/index.css"
-import { mysite } from "../../declarations/mysite";
-
-async function getNum(){
-  const nums = await mysite.getNum();
-  document.getElementById("thumbsUp").innerText = "点赞 " + greeting;
-}
-
-async function click(){
-  mysite.thumbsUp()
-}
